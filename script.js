b290764cadd862e2bd3ab55cf5fa6abecb95174d
unagatacuriosa <gabrielacanalesrubio@gmail.com>
2023-02-23 13:05:41 +0100

    Añadir funcionalidad de guardar el highscore

--- script.js
+++ script.js
@@ -9,15 +9,14 @@ const numberField = document.querySelector('.number')
 
 // 2º paso: Crear las variables que necesitamos
 let score
-let highscore = 0
-let localStore
+let highscore
 
 // Obtener número aleatorio
 const MIN_NUMBER = 1
 const MAX_NUMBER = 20
 let secretNumber
 
-fnAgainIniApp()
+fnIniApp()
 
 // 3º Capturar el checkButton y agregarle un evento y mostrar un mensaje
 checkButton.addEventListener('click', fnCheckButton)
@@ -34,17 +33,18 @@ function fnCheckButton() {
   if (number === secretNumber) {
     messageField.textContent = 'WIN!'
 
-    localStore = localStorage.setItem('highscore', highscore)
-    console.log(localStore)
-
-    if (score > highscore) highscore = highscoreField.textContent = score
+    if (score > highscore) {
+      // Actualizar el highscore en el DOM y en el localStorage
+      highscore = highscoreField.textContent = score
+      localStorage.setItem('highscore', score)
+    }
     // Cambiar el color del fondo o del body o lo que sea
     numberField.textContent = secretNumber
-    document.body.style.backgroundColor = 'LimeGreen'
+    document.body.style.backgroundColor = '#004900'
   } else if (score === 1) {
     mostarMensaje('YOU LOSE')
     scoreField.textContent = 0
-    document.body.style.backgroundColor = 'Crimson'
+    document.body.style.backgroundColor = '#7E041A'
   } else {
     const mensaje =
       number < secretNumber ? 'El número es mayor' : 'El número es menor'
@@ -55,18 +55,14 @@ function fnCheckButton() {
 }
 
 // Otra vez
-againButton.addEventListener('click', fnAgainIniApp)
+againButton.addEventListener('click', fnIniApp)
 
-function fnAgainIniApp() {
-  if (localStore == 20) {
-    highscore = 20
-  } else {
-    highscore = localStorage.getItem('highscore')
-    console.log(highscore)
-  }
+function fnIniApp() {
   score = 20
   scoreField.textContent = score
   guessField.value = ''
+  highscore = localStorage.getItem('highscore') || 0
+  highscoreField.textContent = highscore
   secretNumber = Math.trunc(Math.random() * MAX_NUMBER) + MIN_NUMBER
   mostarMensaje('Empieza a adiviar...')
   document.body.style.backgroundColor = '#222'
